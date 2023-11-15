8d2ddfd0b0703cfbc6c3ebdab901f3fccd73296d
khanhnguyenvinh2002 <giakhanhvy@gmail.com>
2023-11-15 17:50:27 -0500

    update

--- version.py
+++ version.py
@@ -1,10 +1,10 @@
-short_name = "godot"
-name = "Godot Engine"
+short_name = "novacore"
+name = "NovaCore Engine"
 major = 4
 minor = 2
 patch = 0
 status = "dev"
 module_config = ""
 year = 2023
-website = "https://godotengine.org"
+website = "https://hyperedgelabs.xyz"
 docs = "latest"
